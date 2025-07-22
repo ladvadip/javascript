6e65188938881469b4d5febbe751d469a869e2fa
ladvadip <110737969+ladvadip@users.noreply.github.com>
2025-07-22 06:43:28 +0000

    commit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+let name = "deep"
+console.log(name);
+
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+
+let str1 = "deep"
+
+let str2 = " ladva"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(2 > 1);
+console.log(2<1);
+
+
+const myFunction = function()
+{
+    console.log("hello world")
+}
+
+console.log(typeof myFunction)
+
+
---
+++ 01_basics/string.js
@@ -0,0 +1,43 @@
+// const name = " eep "
+
+// console.log(name[0])
+
+// console.log(name. __proto__)
+
+// console.log(name.length)
+
+
+// console.log(name.toUpperCase())
+
+// console.log(name.charAt(3))
+
+// gamename = " valorant " 
+
+// console.log(gamename.trim())
+
+
+// const subject = "java"
+
+// console.log(subject.replace('java','python');
+
+const balance = new Number(10000000000)
+
+console.log(balance);
+
+console.log(balance.toLocaleString())
+
+console.log(balance.toPrecision(3));
+
+
+console.log(Math.min(2,4,1,3,4,5,5));
+
+console.log(Math.random());
+
+
+const min = 10
+const max = 50
+
+Math.random(min,max)
+
+
+
